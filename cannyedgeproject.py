ad955bad290b2409992bae3c95a90f8fe510093e
Henrique <hentt30@gmail.com>
2019-06-22 18:50:21 -0400

    implementation of new methods

--- cannyedgeproject.py
+++ cannyedgeproject.py
@@ -22,6 +22,76 @@ class cannyEdgeDetector:
         self.lowThreshold = lowthreshold
         self.highThreshold = highthreshold
         return
+    def gaussian_kernel(self,size, sigma =1):
+        size = int(size)//2
+        x, y = np.mgrid[-size:size+1, -size:size+1]
+        normal = 1 / (2.0*np.pi*sigma**2)
+        g = np.exp(-((x**2 + y**2)/(2.0*sigma**2)))*normal
+        return g
+    def non_max_supression(self,img,D):
+        M, N = img.shape
+        Z = np.zeros((M,N), dtype = np.int32)
+        angle = D*18-. / np.pi
+        angle[angle<0] += 180
+
+        
+
+        for i in range(1,M-1):
+            for j in range(1,N-1):
+                try:
+                    q = 255
+                    r = 255
+
+                    #angle 0
+                    if( 0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                        q = img[i,j+1]
+                        r = img[i,j-1]
+                    #angle 45
+                    elif (22.5 <= angle[i,j] < 67.5):
+                        q = img[i+1, j-1]
+                        r = ing[i-1, j+1]
+                    #angle 90
+                    elif(67.5 <= angle[i,j] < 112.5):
+                        q = img[i+1, j]
+                        r = img[i-1, j]
+                    #angle 135
+                    elif(112.5 <= angle[i,j] < 157.5):
+                        q = img[i-1,j-1]
+                        r = img[i+1,j+1]
+                    if (img[i,j] >= q) and (img[i,j] >= r):
+                        Z[i,j] = img[i,j]
+                    else:
+                        Z[i,j] = 0
+                expect IndexError as e:
+                    pass
+        return Z
+
+
+        def hysteresis(self, img):
+
+            M,N = img.shape
+            weak = self.weak_pixel
+            strong = self.strong_pixel
+
+            for i in range(1, M-1):
+                for j in range(1,N-1):
+                    if(img[i,j] == weak):
+                        try:
+                            if((img[i+1,j-1] == strong) or (img[i+1,j] == strong) or
+                            (img[i+1,j+1] == strong) or (img[i,j-1] == strong) or
+                            (img[i,j+1] == strong) or (img[i-1,j-1] == strong) or
+                            (img[i-1,j] == strong) or (img[i-1,j+1] == strong)):
+                                img[i,j] = strong
+                            else:
+                                img[i,j] = 0
+                        except IndexError as e:
+                            pass
+            return img
+
+
+
+
+
 
     def sobel_filters(self, img):
         Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
